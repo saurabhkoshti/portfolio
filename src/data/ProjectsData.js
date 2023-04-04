0e914d0c8c7116ebf8e5e06048dd6227c1386b4a
saurabhkoshti <skoshti.1409@gmail.com>
2023-04-04 17:13:49 +0530

    new

--- src/data/ProjectsData.js
+++ src/data/ProjectsData.js
@@ -1,5 +1,6 @@
 
 import RestCountries from "../assets/images/portfolio/RestCountries.jpeg";
+import shopper from "../assets/images/portfolio/shopper.jpg";
 
 const ProjectsPreviewData = [
 
@@ -12,6 +13,16 @@ const ProjectsPreviewData = [
     source: "https://github.com/saurabhkoshti/Countries",
     description:
       "Countries is a project made with API which also has a color theme switcher feature. You can view the list of all countries and find a specific one via search or by filtering regions. By navigating to the individual country page, you are able to see more details about the country like native name and even border countries.",
+  },
+  {
+    id: "Shopper.",
+    img: shopper,
+    name: "Shopper",
+    stack: [ "< CSS3 />", "< React.js />"],
+    src: "https://saurabhkoshti.github.io/shopper/",
+    source: "https://github.com/saurabhkoshti/shopper",
+    description:
+      "Shopper is the mini project created using React.js. It is MERN stack project. It is e-commerce type website created using React.js , MongoDB, Express js and node js. Using fakestoreapi.",
   },
 ];
 
@@ -27,7 +38,16 @@ const ProjectsData = [
     description:
       "Countries is a project made with API which also has a color theme switcher feature. You can view the list of all countries and find a specific one via search or by filtering regions. By navigating to the individual country page, you are able to see more details about the country like native name and even border countries.",
   },
-  
+  {
+    id: "Shopper.",
+    img: shopper,
+    name: "Shopper",
+    stack: [ "< CSS3 />", "< React.js />"],
+    src: "https://saurabhkoshti.github.io/shopper/",
+    source: "https://github.com/saurabhkoshti/shopper",
+    description:
+      "Shopper is the mini project created using React.js. It is MERN stack project. It is e-commerce type website created using React.js , MongoDB, Express js and node js. Using fakestoreapi.",
+  },
 ];
 
 export { ProjectsData, ProjectsPreviewData };
